8e0c39c7eda1120c76f295f774be54c07eca2aed
growthhuman <growthhuman@gmail.com>
2017-09-02 00:14:32 +0900

    表示OK

--- core/src/jp/techacademy/kenta/imabayashi/jumpactiongame/JumpActionGame.java
+++ core/src/jp/techacademy/kenta/imabayashi/jumpactiongame/JumpActionGame.java
@@ -18,19 +18,5 @@ public class JumpActionGame extends Game {
 		//GameScreenを表示する
 		setScreen(new GameScreen(this));
 	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
+
