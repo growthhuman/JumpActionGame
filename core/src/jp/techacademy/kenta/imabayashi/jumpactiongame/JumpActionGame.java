3a786466d19e52016aead30dd0a9afb9f257c5db
growthhuman <growthhuman@gmail.com>
2017-09-01 23:43:22 +0900

    Initial commit

---
+++ core/src/jp/techacademy/kenta/imabayashi/jumpactiongame/JumpActionGame.java
@@ -0,0 +1,36 @@
+package jp.techacademy.kenta.imabayashi.jumpactiongame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class JumpActionGame extends Game {
+	public SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+
+		//GameScreenを表示する
+		setScreen(new GameScreen(this));
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
